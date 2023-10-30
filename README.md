42628fb49707ef6346c380fc4658424d33479b70
Rodrigo-Martins-Mateus <rodrigomartinsmateus@gmail.com>
2023-10-30 17:05:07 -0300

    Inicio

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# App-Receita-Login
+  # Calculadora de churrasco
+  <img src="/images/logo2.png"><br>
+  Antes de tudo, esse projeto teve como finalidade produzir um app de calculadora de churrasco, tema proposto pelo Professor Atila da instituição Senai Suiço Brasileira.<br><br>
+
+  ## Como Rodar o APP
+  Primeiramente vá ao terminal e dê 'npm i/npm install'<br>
+  Logo depois, no terminal mesmo, dê 'npm expo start --tunnel' que irá fornecer um qr code.<br>
+  Para rodar esse QrCode você deve ir ao seu aparelho celular e baixar pela playstore o 'Expo', nele será possível ler o qrCode fornecido.<r>
+  ### Portuguese Version
+  ## Requisitos Funcionais
+  Foi inserido na calculadora o consumo individual e total de carnes e bebidas.<br>
+  cálculo do rateio por pessoa com o churrasco.<br>
+  E exibição do valor total gasto por:<br>
+  - Carnes;<br>
+  - Bebidas;<br>
+  - Material de Consumo;<br>
+  - Locação do local para o evento;(Aqui nós colocamos um mapa da localização do churrasco).
+  
+  <br>É exibido o mapa da Localização do Churrasco, como foi citado acima.<br>
+  Oferecemos no mínimo 3 opções de carne. *Exemplo(Suína, Bovina e Frango)*<br>
+  Oferecemos no mínimo 3 tipos de carne.
+  Exemplo:<br>
+  - Bovina: Coxão Duro, Contra filé, maminha;<br>
+  - Suína: Picanha, linguiça, Paleta;<br>
+  - Frango: coração, Coxa, Asa;<br>
+  
+  Também oferecemos no mínimo 3 opções de Bebidas. *Exemplo(Água, Refrigerante e Cerveja)*<br>
+  Colocamos uma receita para cada tipo de carne afim de ajudar o usuário na escolha de uma receita.<br>
+  E destacamos as informações do local e contato do responsável pelo evento(Falso/Teste).<br><br><br>
+
+  ### English Version
+  ## Functional Requirements
+  The individual and total consumption of meat and drinks was entered into the calculator.<br>
+  calculation of the apportionment per person with the barbecue.<br>
+  And display of the total amount spent by:<br>
+  - Meat;<br>
+  - Drinks;<br>
+  - Consumable Material;<br>
+  - Rental of the venue for the event; (Here we include a map of the barbecue location).
+  
+  <br>The Barbecue Location map is displayed, as mentioned above.<br>
+  We offer at least 3 meat options. *Example(Pork, Beef and Chicken)*<br>
+  We offer at least 3 types of meat.
+  Example:<br>
+  - Beef: Hard thigh, Sirloin, breast;<br>
+  - Pork: Picanha, sausage, Paleta;<br>
+  - Chicken: heart, thigh, wing;<br>
+  
+  We also offer at least 3 drink options. *Example (Water, Soda and Beer)*<br>
+  We have included a recipe for each type of meat to help the user choose a recipe.<br>
+  And we highlight the location and contact information of the person responsible for the event (False/Test).<br><br><br>
+  
+
+
+  
+          
+
+
+
+
---
+++ src/components/receitas/RecipeDetailPage.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, Modal, StyleSheet, ScrollView } from 'react-native';
+
+const RecipeDetailPage = ({ recipe, onClose }) => {
+  return (
+    <Modal visible={true} onRequestClose={onClose}>
+      <View style={styles.container}>
+        
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+        <View style={styles.content}>
+          <Text style={styles.title}>{recipe.title}</Text>
+          <Image source={{ uri: recipe.image }} style={{ width: 300, height: 200,margin: 25, borderRadius: 10 }} />
+          <View style={styles.div}>
+            <Text style={styles.color}>Tempo de Preparo: {recipe.preparationTime}</Text>
+            <Text style={styles.color}>Nível de Dificuldade: {recipe.difficulty}</Text>
+            <Text style={styles.color}>Categoria: {recipe.category} </Text>
+          </View>
+          
+          <View style={styles.divIngredientes}>
+          <Text style={{ fontWeight: 'bold', color: '#fff' }}>
+            {recipe.portion ? `Ingredientes (${recipe.portion} porções)` : 'Ingredientes'} {/* Verifica se existe o recipe.portion */}
+          </Text>
+          {recipe.ingredients.map((section, index) => (
+            <View key={index}>
+              <Text style={{ fontWeight: '500', marginTop: 10, color: '#fff' }}>{section.section}</Text>
+              <Text style={{ fontWeight: '200', marginTop: 10, color: '#fff' }}>{section.items.join('\n')}</Text>
+            </View>
+          ))}
+          </View>
+
+          <View style={styles.divPreparo}>
+          <Text style={{ fontWeight: '500', color: '#fff'}}>Modo de Preparo</Text>
+          {recipe.instructions.map((section, index) => (
+            <View key={index}>
+              <Text style={{ fontWeight: '500', marginTop: 10, color: '#fff' }}>{section.section}</Text>
+              <Text style={{ fontWeight: '200', marginTop: 10, color: '#fff' }}>{section.items.join('\n')}</Text>
+            </View>
+          ))}
+          </View>
+          <View style={styles.buttonClose}>
+          <TouchableOpacity onPress={onClose} style={styles.divClose}>
+            <Text style={styles.color}>Fechar</Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+        </ScrollView>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  title: {
+    fontSize: 17,
+    padding: 5,
+    fontWeight: 'bold',
+    color:'#A52A2A',
+  },
+  div: {
+    backgroundColor: '#A52A2A',
+    margin: 15,
+    elevation: 2,
+    alignItems: 'center',
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderRadius: 5,
+    
+  },
+  divIngredientes: {
+    backgroundColor: '#A52A2A',
+    margin: 15,
+    elevation: 2,
+    paddingLeft: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderRadius: 5,
+  },
+  divPreparo: {
+    backgroundColor: '#A52A2A',
+    margin: 15,
+    elevation: 2,
+    paddingLeft: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderRadius: 5,
+  },
+  divClose:{
+    backgroundColor: '#A52A2A',
+    margin: 15,
+    elevation: 2,
+    paddingLeft: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderRadius: 5,
+    width: 65,
+  },
+  contentContainer: {
+    flexGrow: 1,
+    paddingBottom: 20,
+  },
+  color: {
+    color: '#fff',
+  },
+  buttonClose: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf:'center',
+  }
+});
+
+export default RecipeDetailPage;
